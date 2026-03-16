f84548f02bd7e5b518fd5912d74d0d36e2355d81
Alan Graham <ag@zerohalo.com>
2026-03-16 14:11:55 -0400

    update counts of api trivia

--- internal/admin/admin.go
+++ internal/admin/admin.go
@@ -1124,6 +1124,7 @@ func (s *Store) handleDashboard(w http.ResponseWriter, r *http.Request) {
 	s.mu.RLock()
 	na := len(s.announcements)
 	nt := len(s.triviaItems)
+	ntAPI := len(s.triviaAPIItems)
 	musicStreams := make([]StreamEntry, len(s.streams))
 	copy(musicStreams, s.streams)
 	slide := s.slideDur
@@ -1247,7 +1248,7 @@ func (s *Store) handleDashboard(w http.ResponseWriter, r *http.Request) {
 <tr><td>Announcement duration</td><td style="color:#FFFF00">%s</td></tr>
 <tr><td>Announcement interval</td><td style="color:#FFFF00">%s</td></tr>
 <tr><td colspan="2" style="color:#FFFF00; letter-spacing:1px; padding:10px 0 2px"><b>TRIVIA</b></td></tr>
-<tr><td>Trivia questions</td><td style="color:#FFFF00">%d items</td></tr>
+<tr><td>Trivia questions</td><td style="color:#FFFF00">%d built-in + %d API</td></tr>
 <tr><td>Trivia duration</td><td style="color:#FFFF00">%s</td></tr>
 <tr><td>Trivia interval</td><td style="color:#FFFF00">%s</td></tr>
 <tr><td>Randomize</td><td style="color:#FFFF00">%s</td></tr>
@@ -1282,7 +1283,7 @@ setInterval(function() {
 		fmtUptime(time.Since(s.startedAt)), loadAvgStr, cpuPctStr,
 		clockDisplay, unitDisplay, satDisplay, yn(realisticMoon), slide,
 		na, annD, fmtInterval(annInt),
-		nt, trivD, fmtInterval(trivInt), yn(trivRand), yn(trivBuiltin), trivAPIDetail,
+		nt, ntAPI, trivD, fmtInterval(trivInt), yn(trivRand), yn(trivBuiltin), trivAPIDetail,
 		musicCell,
 		apiStatsHTML,
 		pipelineHTML)
